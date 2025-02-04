fea0e8b029c4e419c37192bae353a12ea6d1f8de
xxh296 <140650758+xxh296@users.noreply.github.com>
2025-02-04 10:07:20 -0800

    Add `toggleIsReadStatus` on `Book.prototype` * Added a function to flip a book's `isRead` boolean.

--- script.js
+++ script.js
@@ -16,24 +16,35 @@ function addBookToLibrary(author, title, pages, isRead, comment) {
 }
 
 function removeBookFromLibrary(bookNum){
+
   //DEBUG
   console.log("myLibrary.lenth === " + myLibrary.length);
+
     const indexToRemove = myLibrary.findIndex(i => i.bookId === bookNum);
     if (indexToRemove !== -1){
       myLibrary.splice(indexToRemove, 1);
     }
+
     //DEBUG
     console.log(myLibrary);
     console.log("myLibrary.lenth === " + myLibrary.length);
+
 }
+
+Book.prototype.toggleIsReadStatus = function(){
+  if (!this.isRead){
+    this.isRead = true;
+  } else if (this.isRead){
+    this.isRead = false;
+  }
+} 
 
 // DEBUG
 // console.log("Hello");
-//populate
+//populate & print
 addBookToLibrary("Author One", "Hello title one", 310, true, "1 this is a comment")
 addBookToLibrary("Author Two", "Hello title two", 3200, false, "2 this is a comment")
 addBookToLibrary("Author three", "Hello title three", 3300, true, "3 this is a comment")
 addBookToLibrary("Author four", "Hello title four", 340, false, "4 this is a comment")
 addBookToLibrary("Author five", "Hello title five", 350, true, "5 this is a comment")
-
 console.log(myLibrary);
