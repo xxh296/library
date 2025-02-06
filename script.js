46bc96f312c8bedf1d965087f8a7c516ae406b47
xxh296 <140650758+xxh296@users.noreply.github.com>
2025-02-05 22:05:37 -0800

    Clean up unneeded comments and debug code

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ const addBookButton = document.createElement("button");
 addBookButton.innerText = "add a book";
 topContainer.appendChild(addBookButton);
 
-
-
 // replace the button with the form
 addBookButton.addEventListener("click", () => {
   addBookButton.remove();
@@ -51,29 +49,16 @@ function addBookToLibrary(author, title, pages, isRead, comment) {
   const book = new Book(author, title, pages, isRead, comment);
   myLibrary.push(book);
   populate();
-  // return myLibrary;
 }
 
 function removeBookFromLibrary(bookNum){
-
-  //DEBUG
-  // console.log("myLibrary.lenth === " + myLibrary.length);
-
     const indexToRemove = myLibrary.findIndex(i => i.bookId === bookNum);
     if (indexToRemove !== -1){
       myLibrary.splice(indexToRemove, 1);
     }
 
     populate();
-
-    //DEBUG
-    // console.log(myLibrary);
-    // console.log("myLibrary.lenth === " + myLibrary.length);
-
 }
-
-// Book.prototype.removeBookFromLibrary = function(){
-// }
 
 Book.prototype.toggleIsReadStatus = function(){
   if (!this.isRead){
@@ -91,49 +76,6 @@ function populate(){
     elementsToRemove.forEach(element => {
       element.remove();
     });
-    
-    // append myLibrary items to .data-table
-
-    // for (let i = 0; i < myLibrary.length; i++) {
-    //   const dataPieceDiv0 = document.createElement("div");
-    //   dataPieceDiv0.textContent = myLibrary[i].author;
-    //   dataTable.appendChild(dataPieceDiv0);
-
-    //   const dataPieceDiv1 = document.createElement("div");
-    //   dataPieceDiv1.textContent = myLibrary[i].title;
-    //   dataTable.appendChild(dataPieceDiv1);
-
-    //   const dataPieceDiv2 = document.createElement("div");
-    //   dataPieceDiv2.textContent = myLibrary[i].pages;
-    //   dataTable.appendChild(dataPieceDiv2);
-
-    //   const dataPieceDiv3 = document.createElement("div");
-    //   dataPieceDiv3.textContent = myLibrary[i].isRead;
-    //   dataTable.appendChild(dataPieceDiv3);
-
-    //   const dataPieceDiv4 = document.createElement("div");
-    //   dataPieceDiv4.textContent = myLibrary[i].comment;
-    //   dataTable.appendChild(dataPieceDiv4);      
-    // }
-
-    // for (let i = 0; i < myLibrary.length; i++) {
-    //   const dataPieces = [
-    //     i + 1 + ".",
-    //     myLibrary[i].author,
-    //     myLibrary[i].title,
-    //     myLibrary[i].pages,
-    //     myLibrary[i].isRead,
-    //     myLibrary[i].comment
-    //   ];
-    
-    //   // Loop through the data pieces and create the divs dynamically
-    //   dataPieces.forEach((dataPiece) => {
-    //     const dataPieceDiv = document.createElement("div");
-    //     dataPieceDiv.textContent = dataPiece;
-    //     dataTable.appendChild(dataPieceDiv);
-    //   });
-    // }
-
 
     for (let i = 0; i < myLibrary.length; i++) {
       const dataPieces = [
@@ -179,8 +121,7 @@ function populate(){
 }
 
 // DEBUG
-// console.log("Hello");
-//populate & print
+//populate
 addBookToLibrary("Author One", "Hello title one", 310, true, "1 this is a comment")
 addBookToLibrary("Author Two", "Hello title two", 3200, false, "2 this is a comment")
 addBookToLibrary("Author three", "Hello title three", 3300, true, "3 this is a comment")
